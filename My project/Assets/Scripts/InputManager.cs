0dae6e30a5c73eae7cda0969d0dd101da95b045e
Nickouwen <71842732+Nickouwen@users.noreply.github.com>
2025-03-02 21:53:27 -0800

    Tried tweaking movement; Unsuccessful

--- My project/Assets/Scripts/InputManager.cs
+++ My project/Assets/Scripts/InputManager.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent<float> OnDrag = new UnityEvent<float>();
+    public float horizontal;
+
+
     void Update()
     {
         Vector3 inputVector = Vector3.zero;
@@ -13,8 +18,12 @@ public class InputManager : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) inputVector += Vector3.right;
         if (Input.GetKey(KeyCode.A)) inputVector += Vector3.left;
         if (Input.GetKeyDown(KeyCode.Space)) inputVector += Vector3.up;
-        if (Input.GetKeyDown(KeyCode.LeftShift)) inputVector += Vector3.forward * 1000;
+        if (Input.GetKeyDown(KeyCode.LeftShift)) inputVector += Vector3.forward;
+
+
+        horizontal = Input.GetAxis("Mouse X");
 
         OnMove?.Invoke(inputVector);
+        OnDrag?.Invoke(horizontal);
     }
 }
--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRigidBody;
     [SerializeField] float playerSpeed = 2000;
-    [SerializeField] float jumpHeight = 100;
+    [SerializeField] float jumpHeight = 10;
     [SerializeField] bool onGround = true;
     [SerializeField] bool canJump = true;
 
-    public void movePlayer(Vector3 input)
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void MovePlayer(Vector3 input)
     {
         Vector3 inputXZPlane = new Vector3(input.x, 0, input.z).normalized;
         if (onGround)
@@ -23,7 +28,14 @@ public class PlayerController : MonoBehaviour
                 canJump = false;
             }
         }
-        playerRigidBody.AddForce(inputXZPlane * playerSpeed * Time.deltaTime);
+        playerRigidBody.AddForce(transform.forward * inputXZPlane.z * playerSpeed * Time.deltaTime);
+        playerRigidBody.AddForce(transform.right * inputXZPlane.x * playerSpeed * Time.deltaTime);
+        playerRigidBody.AddForce(transform.up * inputXZPlane.y * playerSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    public void RotatePlayer(float input)
+    {
+        transform.Rotate(Vector3.up * input);
     }
 
     private void OnCollisionEnter(Collision collision)
